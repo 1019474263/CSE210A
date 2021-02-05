cac7e9a8604972ce03d48ddc6c2802dba6a1985c
Zhengkai Jiang <justinjiang1201@gmail.com>
2021-02-04 21:26:42 -0800

    fixed all issues in easy test script

--- HW4/cse210A-asgtest-hw4-whiless/while.py
+++ HW4/cse210A-asgtest-hw4-whiless/while.py
@@ -2,7 +2,9 @@ import sys
 from lark import Lark
 
 state = {}
-
+rest_of_tree = None
+count = 0
+skip_only = False
 calc_grammar = """
     ?start: aexp
         | bexp
@@ -13,7 +15,8 @@ calc_grammar = """
         | aexp "-" aexp         -> sub
         | aexp "*" aexp         -> mul
         | aexp "/" aexp         -> div
-        | "-" aexp              -> neg
+        | "-(" aexp ")"         -> neg
+        | "-" atom              -> neg_atom
         | "(" aexp ")"          -> apar
 
     ?bexp: "true"               -> true
@@ -78,22 +81,35 @@ def aexp(tree):
             print("Error: can not divide by zero!")
     elif tree.data == 'neg':
         return - aexp(tree.children[0])
+    elif tree.data == 'neg_atom':
+        return - atom(tree.children[0])
     elif tree.data == 'apar':
         return aexp(tree.children[0])
+
 
 def show_aexp(tree):
     if tree.data == 'atom':
         return show_atom(tree.children[0])
     elif tree.data == 'add':
-        return "(" + show_aexp(tree.children[0]) + "+" + show_aexp(tree.children[1])
+        return "(" + show_aexp(tree.children[0]) + "+" + show_aexp(tree.children[1]) + ")"
     elif tree.data == 'sub':
-        return "(" + show_aexp(tree.children[0]) + "-" + show_aexp(tree.children[1])
+        return "(" + show_aexp(tree.children[0]) + "-" + show_aexp(tree.children[1]) + ")"
     elif tree.data == 'mul':
-        return "(" + show_aexp(tree.children[0]) + "*" + show_aexp(tree.children[1])
+        return "(" + show_aexp(tree.children[0]) + "*" + show_aexp(tree.children[1]) + ")"
+    elif tree.data == 'neg':
+        return "-" + show_aexp(tree.children[0])
+    elif tree.data == 'neg_atom':
+        return "-" + show_atom(tree.children[0])
+    elif tree.data == 'apar':
+        return show_aexp(tree.children[0])
 
 
 def show_atom(token):
-    return token.value
+    if token.value.isnumeric():
+        return str(int(token.value))
+    else:
+        return token.value
+
 
 def atom(token):
     if token.type == 'NUMBER':
@@ -109,7 +125,9 @@ def atom(token):
 def assign_var(name, tree):
     state[name] = aexp(tree)
 
+
 def show_assign(var, value):
+    return var + " := " + show_aexp(value)
 
 
 def bexp(tree):
@@ -131,31 +149,82 @@ def bexp(tree):
         return bexp(tree.children[0])
 
 
+def show_bexp(tree):
+    if tree.data == 'true':
+        return "true"
+    elif tree.data == 'false':
+        return 'false'
+    elif tree.data == 'equal':
+        return "(" + show_aexp(tree.children[0]) + "=" + show_aexp(tree.children[1]) + ")"
+    elif tree.data == 'leq':
+        return "(" + show_aexp(tree.children[0]) + "<" + show_aexp(tree.children[1]) + ")"
+    elif tree.data == 'not_op':
+        return "¬" + show_bexp(tree.children[0])
+    elif tree.data == 'and_op':
+        return "(" + show_bexp(tree.children[0]) + "∧" + show_bexp(tree.children[1]) + ")"
+    elif tree.data == 'or_op':
+        return "(" + show_bexp(tree.children[0]) + "∨" + show_bexp(tree.children[1]) + ")"
+    elif tree.data == 'bpar':
+        return show_bexp(tree.children[0])
+
+
 def if_op(condition, exp1, exp2):
     if bexp(condition):
+        print_show(show(exp1))
         eval(exp1)
     else:
+        print_show(show(exp2))
         eval(exp2)
 
 
+def show_if(condition, exp1, exp2):
+    return "if " + show_bexp(condition) + " then { " + show(exp1) + " } else { " + show(exp2) + " }"
+
+
 def while_op(condition, exp):
+
     while bexp(condition):
+        print_show(f"{show(exp)}; {show_while(condition, exp)}")
         eval(exp)
+        print_show(f"skip; {show_while(condition, exp)}")
+        print_show(f"{show_while(condition, exp)}")
+
+
+
+def show_while(condition, exp):
+    return "while " + show_bexp(condition) + " do { " + show(exp) + " }"
 
 
 def bracket(commands):
     for each in commands:
         eval(each)
 
+def show_bracket(commands):
+    output = ""
+    for each in commands:
+        output += show(each) + "; "
+    if len(output):
+        return output[:-2]
+    return output
+
 
 def eval(tree):
+    global skip_only
     if tree.data == 'assign_var':
         assign_var(tree.children[0].value, tree.children[1])
-    elif tree.data == 'skip':
-        pass
     elif tree.data == 'sequence':
-        eval(tree.children[0])
-        eval(tree.children[1])
+        if tree.children[1].data == 'assign_var' and tree.children[0].data == 'bracket':
+            seq = tree.children[0].children[0]
+            tree.children[0] = seq.children[0]
+            seq.children[0] = seq.children[1]
+            seq.children[1] = tree.children[1]
+            tree.children[1] = seq
+            eval(tree)
+        else:
+            eval(tree.children[0])
+            print_show(f"skip; {show(tree.children[1])}")
+            print_show(show(tree.children[1]))
+            eval(tree.children[1])
     elif tree.data == 'if_op':
         if_op(tree.children[0], tree.children[1], tree.children[2])
     elif tree.data == 'while_op':
@@ -163,37 +232,60 @@ def eval(tree):
     elif tree.data == 'bracket':
         bracket(tree.children)
     elif tree.data == 'skip_op':
+        if count == 0:
+            print("")
+            skip_only = True
         pass
     else:
         raise KeyError()
 
 
-
-
-
 def show(tree):
     if tree.data == 'assign_var':
-        show_assign(tree.children[0].value, tree.children[1])
+        return show_assign(tree.children[0].value, tree.children[1])
+    elif tree.data == 'sequence':
+        return show(tree.children[0]) + "; " + show(tree.children[1])
+    elif tree.data == 'if_op':
+        return show_if(tree.children[0], tree.children[1], tree.children[2])
+    elif tree.data == 'while_op':
+        return show_while(tree.children[0], tree.children[1])
+    elif tree.data == 'bracket':
+        return show_bracket(tree.children)
+    elif tree.data == 'skip_op':
+        return ""
+    else:
+        raise KeyError()
+
+
+def print_show(code):
+    global count
+    count += 1
+    print(f"⇒ {code}, {{{get_state()}}}")
+    if count > 9999:
+        quit()
+
+
+def get_state():
+    output = ""
+    keys = state.keys()
+    keys = sorted(keys)
+    for each in keys:
+        output += f"{each} → {state[each]}, "
+    if len(output):
+        output = output[:-2]
+    return output
+
 
 def main():
     # for line in sys.stdin:
     #     tree = calc(line)
     #     eval(tree)
-    #     keys = state.keys()
-    #     keys = sorted(keys)
-    #     output = ""
-    #     for each in keys:
-    #         output += f"{each} → {state[each]}, "
-    #     if len(output) > 0:
-    #         print(f"{{{output[:-2]}}}")
-    #     else:
-    #         print("{}")
-    tree = calc("x:= 1\n")
+    # if not skip_only:
+    #     print_show("skip")
+    tree = calc(" k := ( 49 ) * 3 + k\n")
     print(tree)
-    # for each in tree.iter_subtrees_topdown():
-    #     print(each)
     eval(tree)
-    show(tree)
+    print("⇒ " + show(tree) + ", ")
     print(state)
 
 
